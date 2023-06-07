a5edce3cd915661d019bd19c6cb8586c72270d1d
Al Noman <aalnomanm10@gmail.com>
2023-06-07 09:32:44 -0700

    basic-route-setUp

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }]
+    },
+]);
+
+export default Router;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl btn-primary">home</h1>
+        </div>
+    );
+};
+
+export default Home;
